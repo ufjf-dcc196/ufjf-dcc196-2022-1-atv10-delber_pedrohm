19e400e281ed4ba1ef8da7f00c537aef1a63f387
Delber Soares <delberss@hotmail.com>
2022-08-15 16:17:10 -0300

    Adiciona layout do produto

--- app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
@@ -1,22 +1,19 @@
 package br.ufjf.dcc196.delbersspedrohpmarques.controledeestoque;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerProdutos;
     List<Produto> produtos;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         produtos = new ArrayList<Produto>(){{
             add(new Produto( "Halter", 50.00f,10));
             add(new Produto( "Anilha",45.00f,10));
@@ -24,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         }};
 
         recyclerProdutos = findViewById(R.id.recyclerProdutos);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerProdutos.setLayoutManager(layoutManager);
     }
 }
