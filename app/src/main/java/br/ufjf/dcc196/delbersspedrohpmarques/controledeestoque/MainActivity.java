b24bb6d74524918acd98dc0dca4114f7e0542a95
Pedro Henrique Marques <71791347+PedroHPMarques@users.noreply.github.com>
2022-08-15 16:23:54 -0300

    Chamando o produtoAdapter na main

--- app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
@@ -7,21 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerProdutos;
     List<Produto> produtos;
+    private ProdutoAdapter produtoAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        produtos = new ArrayList<Produto>(){{
-            add(new Produto( "Halter", 50.00f,10));
-            add(new Produto( "Anilha",45.00f,10));
-            add(new Produto( "Barra", 100f,5));
-        }};
-
         recyclerProdutos = findViewById(R.id.recyclerProdutos);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerProdutos.setLayoutManager(layoutManager);
+
+        produtoAdapter = new ProdutoAdapter(produtos);
+        recyclerProdutos.setAdapter(produtoAdapter);
     }
 }
