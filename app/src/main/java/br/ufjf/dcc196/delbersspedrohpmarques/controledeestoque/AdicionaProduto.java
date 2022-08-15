dfed516a89ea8ce3f1691f45b25ccb6ef8e3c228
Pedro Henrique Marques <71791347+PedroHPMarques@users.noreply.github.com>
2022-08-15 16:41:23 -0300

    Activity para adicionar novos produtos

---
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/AdicionaProduto.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.delbersspedrohpmarques.controledeestoque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionaProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adiciona_produto);
+    }
+}
