832a97e40bdc3eb85bf99195f692c017fae7cbd8
Delber Soares <delberss@hotmail.com>
2022-08-15 16:15:08 -0300

    Adiciona fonte de dados

--- app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
@@ -5,13 +5,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerProdutos;
+    List<Produto> produtos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        produtos = new ArrayList<Produto>(){{
+            add(new Produto( "Halter", 50.00f,10));
+            add(new Produto( "Anilha",45.00f,10));
+            add(new Produto( "Barra", 100f,5));
+        }};
 
         recyclerProdutos = findViewById(R.id.recyclerProdutos);
     }
---
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/Produto.java
@@ -0,0 +1,37 @@
+package br.ufjf.dcc196.delbersspedrohpmarques.controledeestoque;
+
+public class Produto {
+    private String nome;
+    private Float preco;
+    private Integer quantidade;
+
+    public Produto(String nome, Float preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
