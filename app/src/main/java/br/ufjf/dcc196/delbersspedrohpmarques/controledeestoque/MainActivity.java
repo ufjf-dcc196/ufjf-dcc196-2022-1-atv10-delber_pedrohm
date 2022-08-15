3970812be432d3693250dbb21fa8eaecc8c48726
Delber Soares <delberss@hotmail.com>
2022-08-15 16:10:31 -0300

    Adiciona recyclerView

--- app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delbersspedrohpmarques/controledeestoque/MainActivity.java
@@ -1,14 +1,18 @@
 package br.ufjf.dcc196.delbersspedrohpmarques.controledeestoque;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerProdutos = findViewById(R.id.recyclerProdutos);
     }
 }
